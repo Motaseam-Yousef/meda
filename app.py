b20b55121fa99e1a7b5c929d136b433650d8cc1f
Motaseam Yousef <motaseamsaher@gmailcom>
2024-05-24 17:16:39 +0300

    add doc2

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import google.generativeai as genai
 import io
 from io import BytesIO
 from PIL import Image
+from openai import OpenAI
+import base64
+import time
 
 def generate_content(img=None):
     load_dotenv()
@@ -13,14 +16,49 @@ def generate_content(img=None):
     model = genai.GenerativeModel('gemini-1.0-pro-vision-latest')
     try:
         response = model.generate_content([img])
-        # analysis model
+        
+        # Add a delay of 500ms
+        time.sleep(0.5)
+        
+        # Analysis model
         model_ana = genai.GenerativeModel('gemini-1.5-pro-latest')
         prompt_ana = f'''if this text is a medical report, X-Ray, MRI (Magnetic Resonance Imaging), CT (Computed Tomography), Ultrasound, PET (Positron Emission Tomography), SPECT (Single Photon Emission Computed Tomography), Mammography, Fluoroscopy, DEXA (Dual-Energy X-ray Absorptiometry) analysis: \n"{response.text}" \n\n give me more explain for the results and sure to say "الرجاء التواصل مع طبيب لمعلومات أكثر" Answer ONLY in Arabic.
         else (The given Text not related to any medical information) then reponse "لا يمكنني المساعدة بذلك"'''
         response_ana = model_ana.generate_content([prompt_ana])
-        return response_ana.text 
+        return response_ana.text, response.text
     except Exception as e:
         st.error("Failed to generate content: {}".format(e))
+        return None, None
+
+def encode_image(image):
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+def generate_gpt_response(image):
+    try:
+        base64_image = encode_image(image)
+        openai_api = os.getenv('OPENAI_API')
+        openai_client = OpenAI(api_key=openai_api)
+        MODEL = 'gpt-4o'
+
+        response = openai_client.chat.completions.create(
+            model=MODEL,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant in healthcare and medical reports and images"},
+                {"role": "user", "content": [
+                    {"type": "text", "text": '''If this image is a medical report, X-ray, MRI (Magnetic Resonance Imaging), CT (Computed Tomography), Ultrasound, PET (Positron Emission Tomography), SPECT (Single Photon Emission Computed Tomography), Mammography, Fluoroscopy, or DEXA (Dual-Energy X-ray Absorptiometry), or ECG,  please analyze it and provide a detailed explanation of whether the results are good or not. Explain the reasons behind your assessment, including any specific findings such as "1) 2) etc" and provide more detailed results. Additionally, offer recommendations based on your analysis, Answer ONLY in Arabic.
+                else (The given Text not related to any medical information) then reponse "لا يمكنني المساعدة بذلك'''},
+                    {"type": "image_url", "image_url": {
+                        "url": f"data:image/png;base64,{base64_image}"}
+                    }
+                ]}
+            ],
+    temperature=0.0,
+)
+        return response.choices[0].message.content
+    except Exception as e:
+        st.error("Failed to generate GPT response: {}".format(e))
         return None
 
 def main():
@@ -56,8 +94,12 @@ def main():
 
     if st.button("Analysis"):
         if img:
-            processed_text = generate_content(img)
-            st.markdown(f"<div style='direction: rtl; text-align: lest;'>{processed_text}</div>", unsafe_allow_html=True)
+            processed_text_ahmad, raw_response_ahmad = generate_content(img)
+            processed_text_mohammad = generate_gpt_response(img)
+            st.markdown("### Doctor Ahmad")
+            st.markdown(f"<div style='direction: rtl; text-align: right;'> {processed_text_ahmad}</div>", unsafe_allow_html=True)
+            st.markdown("### Doctor Mohammad")
+            st.markdown(f"<div style='direction: rtl; text-align: right;'> {processed_text_mohammad}</div>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
